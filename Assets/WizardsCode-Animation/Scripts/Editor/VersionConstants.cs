98d67ade705e142df17249681476c479e63140bc
Ross Gardler <ross.gardler@microsoft.com>
2020-06-27 21:59:19 -0800

    Move into own folder and add pacakge manifest config

---
+++ Assets/WizardsCode-Animation/Scripts/Editor/VersionConstants.cs
@@ -0,0 +1,33 @@
+namespace org.WizardsCode.Animator.Editor
+{
+	/// <summary>
+	/// Version info for this library.
+	/// </summary>
+	internal static class VersionConstants
+	{
+		/// <summary>
+		/// The semantic version
+		/// </summary>
+		public const string VERSION = "0.0.1";
+
+		/// <summary>
+		/// The branch of GIT this package was published from.
+		/// </summary>
+		public const string GIT_BRANCH = "master";
+
+		/// <summary>
+		/// The current GIT commit hash this package was published on.
+		/// </summary>
+		public const string GIT_COMMIT = "34e32380371849aed19145f16a1bd1ca90658421";
+
+		/// <summary>
+		/// The UTC human-readable date this package was published at.
+		/// </summary>
+		public const string PUBLISH_DATE = "Sunday, June 28, 2020";
+
+		/// <summary>
+		/// The UTC time this package was published at.
+		/// </summary>
+		public const string PUBLISH_TIME = "06/28/2020 05:49:12";
+	}
+}

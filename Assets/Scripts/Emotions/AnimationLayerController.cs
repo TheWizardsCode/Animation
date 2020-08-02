94710d75df78f9dafdd2b8b3c41d96715386498a
Ross Gardler <ross.gardler@microsoft.com>
2020-08-02 12:33:45 -0800

    Add an UMA prefab for a more realistic character in demo's

--- Assets/Scripts/Emotions/AnimationLayerController.cs
+++ Assets/Scripts/Emotions/AnimationLayerController.cs
@@ -1,5 +1,6 @@
 ﻿#if SALSA
 using CrazyMinnow.SALSA;
+#endif
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,21 +14,29 @@ namespace WizardsCode.Animation
     public class AnimationLayerController : MonoBehaviour
     {
         [SerializeField, Range(0.1f, 4f), Tooltip("The duration of the transition from one weight to the next, if this is too short the animations will look jerky.")]
-        float m_ChangeDuration = 4f;
+        float m_ChangeDuration = 0.5f;
         [SerializeField, Range(0, 1f), Tooltip("The weight for the layer used when the character is deemed to be talking.")]
         float m_TalkingLayerWieght = 1f;
 
         private Animator m_animator;
-        private Salsa m_salsa;
         private float m_GoalWeight;
         private float m_ChangeProgress = 0;
+
+#if SALSA
+        private Salsa m_salsa;
+#else
+        private bool m_IsTalking;
+#endif
 
         private const int TALKING_LAYER_INDEX = 1;
 
         private void Awake()
         {
             m_animator = GetComponentInParent<Animator>();
+
+#if SALSA
             m_salsa = GetComponent<Salsa>();
+#endif
         }
 
         void Update()
@@ -51,13 +60,32 @@ namespace WizardsCode.Animation
             }
         }
 
-        bool isTalking
+#if Salsa
+        public bool isTalking
         {
             get
             {
                 return m_salsa.IsSALSAing;
             }
+
+            set
+            {
+                Debug.LogWarning("Using Salsa to manage LipSync and therefore to manage whether the character is talking or not. Should not be calling isTalking = true/false.");
+            }
         }
+#else
+        public bool isTalking
+        {
+            get
+            {
+                return m_IsTalking;
+            }
+
+            set
+            {
+                m_IsTalking = value;
+            }
+        }
+#endif
     }
 }
-#endif

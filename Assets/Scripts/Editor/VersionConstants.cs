92c8a4091a2f552c97e514b1e14eae075c81ea40
Ross Gardler <ross.gardler@microsoft.com>
2020-08-02 08:18:23 -0800

    bump version number

--- Assets/Scripts/Editor/VersionConstants.cs
+++ Assets/Scripts/Editor/VersionConstants.cs
@@ -8,26 +8,26 @@ namespace org.WizardsCode.Animator.Editor
 		/// <summary>
 		/// The semantic version
 		/// </summary>
-		public const string VERSION = "0.0.3";
+		public const string VERSION = "0.0.4";
 
 		/// <summary>
 		/// The branch of GIT this package was published from.
 		/// </summary>
-		public const string GIT_BRANCH = "master";
+		public const string GIT_BRANCH = "talking";
 
 		/// <summary>
 		/// The current GIT commit hash this package was published on.
 		/// </summary>
-		public const string GIT_COMMIT = "32d28d80c29e1dc4ae57be3fa37ed823f46b9016";
+		public const string GIT_COMMIT = "259ff9ac36e749007bfe8722124b12025cce977c";
 
 		/// <summary>
 		/// The UTC human-readable date this package was published at.
 		/// </summary>
-		public const string PUBLISH_DATE = "Monday, June 29, 2020";
+		public const string PUBLISH_DATE = "Sunday, August 2, 2020";
 
 		/// <summary>
 		/// The UTC time this package was published at.
 		/// </summary>
-		public const string PUBLISH_TIME = "06/29/2020 01:58:04";
+		public const string PUBLISH_TIME = "08/02/2020 16:17:44";
 	}
 }
